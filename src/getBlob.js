739ea7c9972f86b6bd695a1ff22f7874f2bd60f6
8th713 <8th713@gmail.com>
2017-07-17 20:14:41 +0900

    Add CORS Blob fetcher

---
+++ src/getBlob.js
@@ -0,0 +1,37 @@
+// @flow
+/* global GM_xmlhttpRequest */
+import JSZip from 'jszip'
+import * as FileName from './fileName'
+
+function fetch(url: string, responseType: string) {
+  return new Promise((resolve, reject) => {
+    GM_xmlhttpRequest({
+      url,
+      responseType,
+      method: 'GET',
+      headers: { referer: url },
+      onload: (xhr) => resolve(xhr.response),
+      onerror: reject
+    })
+  })
+}
+
+export function fromSingle(src: Source) {
+  const image = src.images[0]
+
+  return fetch(image.src, 'blob')
+}
+
+export async function fromMultiple(src: Source) {
+  const zip = new JSZip()
+
+  for (const image of src.images) {
+    const ext = FileName.getExt(image.src)
+    const name = `${image.alt}.${ext}`
+    const blob = await fetch(image.src, 'blob')
+
+    zip.file(name, blob)
+  }
+
+  return zip.generateAsync({type: 'blob'})
+}
